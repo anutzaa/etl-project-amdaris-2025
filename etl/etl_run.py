a54935a490cbfed66b80b167edba31cd1ed34ba1
anutza <148497221+anutzaa@users.noreply.github.com>
2025-04-16 09:56:40 +0300

    dotenv load in main

--- etl/etl_run.py
+++ etl/etl_run.py
@@ -1,9 +1,13 @@
+from dotenv import load_dotenv
+
 from extract.main_extract import extract
 from transform.main_transform import transform
 from load.main_load import load
 
 
 if __name__ == "__main__":
+    load_dotenv()
+
     extract()
     transform()
     load()
