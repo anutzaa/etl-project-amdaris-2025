ee08652dda3152d7098f64fc8a46fd6fd5efbaf9
anutza <148497221+anutzaa@users.noreply.github.com>
2025-04-22 12:40:30 +0300

    fixed logger file directory after refactor

--- etl/commons/logger.py
+++ etl/commons/logger.py
@@ -3,13 +3,13 @@ import os
 from datetime import datetime
 
 
-def setup_logger(app_name, base_dir="../etl"):
+def setup_logger(app_name, base_dir="etl"):
     """
     Configure and return a logger for the app based on its name.
 
     Parameters:
         app_name -- Name of the app for which the logger is to be configured
-        base_dir -- Base directory where logs will be stored (default is "../etl")
+        base_dir -- Base directory where logs will be stored (default is "etl")
 
     Returns:
         logger -- Configured logger instance for the app
